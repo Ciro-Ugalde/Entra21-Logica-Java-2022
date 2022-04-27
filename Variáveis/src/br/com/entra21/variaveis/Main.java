ab2895746e8af9025e382bf78b7f1eed993b2ab9
Ciro-Ugalde <ciro.daniel.ugalde@gmail.com>
2022-04-27 18:58:26 -0300

    Aula 27/4

---
+++ Variáveis/src/br/com/entra21/variaveis/Main.java
@@ -0,0 +1,28 @@
+package br.com.entra21.variaveis;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		byte idade;
+		idade=33;
+		
+		byte outraIdade = 34;
+		
+		short ano=2022;
+		
+		short anoPassado;
+		
+		anoPassado=2021;
+		
+		System.out.println("Minha idade é "+(idade+outraIdade));
+		
+		System.out.println("Estamos em "+ano+" e ano passado foi "+anoPassado+".");
+		
+		
+
+		
+		
+	}
+
+}
