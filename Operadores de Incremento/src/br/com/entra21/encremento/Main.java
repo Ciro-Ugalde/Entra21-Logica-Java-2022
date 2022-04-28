818f7e9376ba599f0d1112cc2d21499583e013e3
Ciro-Ugalde <ciro.daniel.ugalde@gmail.com>
2022-04-27 22:00:53 -0300

    aula sobre operadores aritmeticos

---
+++ Operadores de Incremento/src/br/com/entra21/encremento/Main.java
@@ -0,0 +1,44 @@
+package br.com.entra21.encremento;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int idade=33;
+		
+		//aniversario
+		idade = idade+1;  //34
+		System.out.println(idade);
+		
+		//ano 29/2 4 em 4 anos
+		
+		idade= idade+4;
+		System.out.println(idade);
+		
+		idade++;
+		System.out.println(idade);
+		
+		idade+=2;
+		System.out.println(idade);
+		
+		idade*=2;
+		System.out.println(idade);
+		
+		idade/=2;
+		System.out.println(idade);
+		
+		idade--;
+		System.out.println(idade);
+		
+		idade-=15;
+		System.out.println(idade);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ PreçoProduto/src/br/com/entra21/precoproduto/Main.java
@@ -0,0 +1,38 @@
+package br.com.entra21.precoproduto;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Faça um programa que solicite o nome e preço de custo de um produto qualquer, após, processe um aumento de 65% e informe qual será o preço de venda.");
+		
+		String nomeProduto;
+		float precoCusto,precoVenda;
+				
+		Scanner entrada= new Scanner(System.in);
+		
+		System.out.println("Qual o nome do produto?");
+		
+		System.out.println("Informe o valor de custo do produto " +nomeProduto+" ?");
+		precoCusto = entrada.nextFloat();
+		
+		precoVenda = entrada.nextFloat();
+		
+		precoVenda = precoCusto + ( precoCusto * 65 / 100);
+		
+		System.out.println("O preço de venda é "+precoVenda); 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+	}
+
+}
